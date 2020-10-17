fe4b3b8dab9ae3df0706c912d3c0b995b65a00d6
DELL <DELL@DESKTOP-0V2A9HG>
2020-10-17 08:21:07 +0530

    Second Commit

--- GIT-Basic/src/App_download.java
+++ GIT-Basic/src/App_download.java
@@ -3,6 +3,14 @@ public class App_download {
 
 	public static void main(String[] args) {
 		System.out.println("GIT HELLo world");
+		
 	}
+public void method1() {
+	System.out.println("Changes made");
+}
+
+public void method2() {
+	System.out.println("Modifications made");
+}
 
 }
